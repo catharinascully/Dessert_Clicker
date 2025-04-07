5df15908541915494613581a106bcd84f951da38
catharinascully <>
2025-04-07 14:28:54 -0300

    commit

--- app/src/main/java/com/example/dessertclicker/model/Dessert.kt
+++ app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -15,4 +15,4 @@
  */
 package com.example.dessertclicker.model
 
-data class Dessert(val imageId: Int, val pricetionAmount: Int)
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
